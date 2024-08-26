402588cdf325b26471e80136e41af748b588dedf
manjula.r <manjula.r@MANJULA.ciindia.com>
2024-08-26 12:49:02 +0530

    commiting test2 to Branch_Sample-Manju

---
+++ src/com/practice/Test2.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("Test 2 Execution");
+		
+		String hungry = "No";
+		if (hungry =="NO") {
+			System.out.println("Goto Work");
+			
+		} else {
+			System.out.println("Go and Eat");
+
+		}
+	}
+}
